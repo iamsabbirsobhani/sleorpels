9fa37a03025e21e9ea00d8eb1b59aba0fdaaea6b
iamsabbirsobhani <sabbirsobhani@hotmail.com>
2022-07-29 23:49:19 +0600

    cors added

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,12 +1,23 @@
 module.exports = [
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::logger',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  "strapi::errors",
+  "strapi::security",
+  {
+    name: "strapi::cors",
+    config: {
+      enabled: true,
+      origin: [
+        "http://localhost:1337",
+        "http://localhost:3000",
+        "https://sleorpels.vercel.app",
+        "https://sleorpels.herokuapp.com",
+      ],
+    },
+  },
+  "strapi::poweredBy",
+  "strapi::logger",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
