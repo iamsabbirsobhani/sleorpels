8630bdaa548106c4372d2fbccb20c67b338216a2
iamsabbirsobhani <sabbirsobhani@hotmail.com>
2022-08-08 21:39:36 +0600

    HomeCoverPic single type table added

---
+++ src/api/home-cover-pic/controllers/home-cover-pic.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  home-cover-pic controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::home-cover-pic.home-cover-pic');
---
+++ src/api/home-cover-pic/routes/home-cover-pic.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * home-cover-pic router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::home-cover-pic.home-cover-pic');
