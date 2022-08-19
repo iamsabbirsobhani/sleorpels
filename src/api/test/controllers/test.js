6ca9bbfae64c96e5733e04c965f866795e07e4e6
iamsabbirsobhani <sabbirsobhani@hotmail.com>
2022-08-19 20:37:44 +0600

    updated

--- src/api/test/controllers/test.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  async index(ctx, next) {
-    // called by GET /hello
-
-    ctx.body = "Hello Strapi"; // we could also send a JSON
-  },
-};
--- src/api/test/routes/test.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  routes: [
-    {
-      method: "GET",
-      path: "/test",
-      handler: "test.index",
-    },
-  ],
-};
