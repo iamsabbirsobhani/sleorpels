3831033ec03248fa1a0bc715a5fc65196e9e6509
iamsabbirsobhani <sabbirsobhani@hotmail.com>
2022-11-03 11:07:31 +0600

    server.js app kyes bug fixed

--- config/server.js
+++ config/server.js
@@ -2,6 +2,6 @@ module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
   app: {
-    keys: env.array("APP_KEYS"),
+    keys: ["2enquxxOM6rs1HsTTtu9cA==", "Nmfbn/WBDT/Qvru8Sae/IA==,edGlQueBxoSIx7G35Zug4Q==", "vNKfdl/lcLlvbHzh+6tzEQ=="],
   },
 });
