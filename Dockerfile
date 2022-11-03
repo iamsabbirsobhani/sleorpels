458cdf47941a3af4d7c1b25e5d5378d25f455083
iamsabbirsobhani <sabbirsobhani@hotmail.com>
2022-11-03 11:38:45 +0600

    docker file updated

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
 FROM node:16-alpine
 WORKDIR /sleorpels
 COPY . .
+COPY .env .env
 RUN yarn install --production && yarn build
 CMD ["npm", "start"]
 EXPOSE 1337
--- config/server.js
+++ config/server.js
@@ -2,6 +2,6 @@ module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
   app: {
-    keys: ["2enquxxOM6rs1HsTTtu9cA==", "Nmfbn/WBDT/Qvru8Sae/IA==,edGlQueBxoSIx7G35Zug4Q==", "vNKfdl/lcLlvbHzh+6tzEQ=="],
+    keys: env.array("APP_KEYS"),
   },
 });
